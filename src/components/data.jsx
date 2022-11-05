59f89f0efc25a84d1d264afbccd4294931ef06b7
jhael07 <jhaelr007@gmail.com>
2022-11-05 16:37:13 -0400

    links actualizados del mapa

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -2,7 +2,7 @@ export default [
   {
     title: "Punta Cana",
     location: "La Altagracia",
-    link: "#",
+    link: "https://www.google.com/maps/place/Punta+Cana/@18.6407175,-68.4713458,11z/data=!3m1!4b1!4m5!3m4!1s0x8ea891645dcbfe77:0x61881cfaed12f6f3!8m2!3d18.5600761!4d-68.372535",
     description:
       "Encontrarás diversión bajo el sol, con fina arena blanca y un mar cristalino e iridiscente para nadar, pescar, o darte un chapuzón entre vida marina y barcos hundidos. Punta Cana, tiene 10 campos de golf ubicados a lo largo de la costa, un escape de playa con lujosas marinas y cenas gourmet, y una zona de bienestar con los mejores spas del país, incluyendo el único Six Senses del Caribe.",
     image:
@@ -11,7 +11,7 @@ export default [
   {
     title: "Parque los haitises",
     location: "Hato Mayor, Monte Plata, Samaná",
-    link: "#",
+    link: "https://www.google.com/maps/place/Los+Haitises+National+Park/@18.9833318,-69.717452,11z/data=!4m9!1m2!2m1!1sparque+los+haitises!3m5!1s0x8eaf1d07a09358d3:0x58e52391a4249225!8m2!3d18.9833318!4d-69.4537801!15sChNwYXJxdWUgbG9zIGhhaXRpc2VzkgENbmF0aW9uYWxfcGFya-ABAA",
     description:
       "El Parque Nacional Los Haitises es una de las joyas del sistema de parques nacionales de República Dominicana. Los Haitises se traduce a “tierra montañosa” en el lenguaje taíno, atrae a múltiples visitantes que vienen en barco para ver su magnífica serie de formaciones rocosas que se elevan fuera del agua. El parque también cuenta con exuberantes manglares a lo largo de su bahía, la cual está adornada con cayos hogar de múltiples colonias de aves y una serie de cuevas conocidas por tener la mayor cantidad de petroglifos y pictografías del país.",
     image:
@@ -20,7 +20,7 @@ export default [
   {
     title: "Dunas de Baní",
     location: "Peravia",
-    link: "#",
+    link: "https://www.google.com/maps/place/Dunas+de+Las+Calderas+Baní/@18.2135947,-70.5335064,17z/data=!3m1!4b1!4m5!3m4!1s0x8ebab6632c1c3a6d:0x430ce2ee4b0e507f!8m2!3d18.2135896!4d-70.5313123",
     description:
       "El maravilloso paisaje de Las Dunas de Baní es un tesoro nacional de la República Dominicana, gracias a sus altas montañas de arena blanca fina y el sol tan cálido como en pocos lugares del país. Esta reserva científica que responde al nombre original de Monumento Natural Félix Servio Ducoudray, es considerada uno de los espacios naturales más hermosos de la isla.",
     image:
