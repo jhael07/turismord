a5a000ff47eaa6d00d18883e69b5c6b4dec73fb2
jhael07 <109443811+jhael07@users.noreply.github.com>
2023-01-06 22:35:29 -0400

    klk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.js";
-import "./app.css";
+import App from "src/app.js";
+import "src/app.css";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
